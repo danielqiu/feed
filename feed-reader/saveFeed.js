39d47f14c1d8099fa4c29314598c09add32e7910
Daniel Qiu <daniel@danielqiu.com>
2014-12-21 10:26:30 -0800

    improves an exception handling

--- feed-reader/saveFeed.js
+++ feed-reader/saveFeed.js
@@ -5,25 +5,28 @@ insertFeedEntry = function (entry, feed) {
         FeedEntries.insert(entry);
         console.log("inserted");
 
-        // store the latest feed date
-        if (typeof feed.latest_date === 'undefined' || feed.latest_date == null 
-            || feed.latest_date < entry.pubdate) {
-            
-            feed.latest_date = entry.pubdate;
-            // update database
-            console.log("updating feed of " + feed._id + " with latest_date: " + entry.pubdate);
-            Feeds.update({
-                _id: feed._id
-            }, {
-                $set: {
-                    last_updated: entry.pubdate,
-                }
-            });
-
-            //update in-memory variable of feed
-            feed.last_updated = entry.pubdate;
-        }
     } catch (e) {
         console.log("Exception: " + e);
     };
+
+    // store the latest feed date
+    console.log("typeof feed.latest_date: " + typeof feed.latest_date);
+    console.log("feed.latest_date: " + feed.latest_date)
+    if (typeof feed.latest_date === 'undefined' || feed.latest_date == null
+        || feed.latest_date < entry.pubdate) {
+
+        feed.latest_date = entry.pubdate;
+        // update database
+        console.log("updating feed of " + feed._id + " with latest_date: " + entry.pubdate);
+        Feeds.update({
+            _id: feed._id
+        }, {
+            $set: {
+                last_updated: entry.pubdate,
+            }
+        });
+
+        //update in-memory variable of feed
+        feed.last_updated = entry.pubdate;
+    }
 }
